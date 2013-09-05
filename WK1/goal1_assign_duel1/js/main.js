00deb54549216d2a90ad2d070bd68f1017b731ec
Bigdsweetz <bigdsweetz@gmail.com>
2013-09-05 13:58:19 -0400

    I have created the js file and following along with the video have created a self executing function. I have also thrown in the function fight and the function winnerCheck.

---
+++ WK1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,27 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Douglas Sweeting
+ * Date: 9/5/13
+ * Time: 1:16 PM
+ * Assignment : Goal 1 Assignment duel 1
+ */
+
+
+
+// Self-executing function
+(function (){
+
+    console.log("!!!Fight!!!");
+ /*
+    var firstPlayerName = "Spawn";
+    var secondPlayerName = ""
+
+
+//This function contains the code that loops through rounds and reduces the player's health
+    function fight();
+
+    /* This function is invoked after each round and returns a string of either the winner, the loser, a tie (both die)
+     or no winner yet. This function does the conditional logic to determine if there is a winner or not.)
+     */
+   /* function winnerCheck();       */
+})
