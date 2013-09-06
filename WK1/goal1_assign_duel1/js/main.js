54cdc7c5a82b63c792ca1a23a5e1e5658487f95d
Bigdsweetz <bigdsweetz@gmail.com>
2013-09-05 22:57:37 -0400

    I have started the minimum damage for players 1&2.

--- WK1/goal1_assign_duel1/js/main.js
+++ WK1/goal1_assign_duel1/js/main.js
@@ -30,6 +30,29 @@
 
      //This function contains the code that loops through rounds and reduces the player's health
      function fight(){
+         console.log("This is the Fight function");
+         alert(firstPlayerName + " : " + firstPlayerHealth + " vs " + secondPlayerName + " : " + secondPlayerHealth + '\n' +
+             "         " +"ROUND " + round + " FIGHT! ");
+
+         for (var i=0; i< 10; i++){
+
+             //Shows the loop is working
+             console.log(i);
+
+             //The minimum damage for players 1&2
+             var minDamage1 = firstPlayerDamage * .5;
+             var minDamage2 = secondPlayerDamage * .5;
+
+             //The Random variables
+             var firstPlayer = Math.floor(Math.random() * (firstPlayerDamage - minDamage1) + minDamage1);
+             var secondPlayer = Math.floor(Math.random() * (secondPlayerDamage - minDamage2) + minDamage2);
+
+             //This is the output for random variables
+             console.log(firstPlayer);
+             console.log(secondPlayer);
+         }
+
+         //winnerCheck()
 
      };
 
@@ -38,8 +61,12 @@
      /* This function is invoked after each round and returns a string of either the winner, the loser, a tie (both die)
      or no winner yet. This function does the conditional logic to determine if there is a winner or not.)
      */
-    function winnerCheck(){};
+    function winnerCheck(){
 
+    };
+
+    /* The program get's started here */
+    console.log("Program starts here");
     fight();
 
 })();
