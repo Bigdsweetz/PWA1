148cc089b41747d5f012d1b57a483d7efb17f171
Bigdsweetz <bigdsweetz@gmail.com>
2013-09-05 21:05:59 -0400

    I added the variables to the file.

--- WK1/goal1_assign_duel1/js/main.js
+++ WK1/goal1_assign_duel1/js/main.js
@@ -12,16 +12,34 @@
 (function (){
 
     console.log("!!!Fight!!!");
- /*
-    var firstPlayerName = "Spawn";
-    var secondPlayerName = ""
 
 
-//This function contains the code that loops through rounds and reduces the player's health
-    function fight();
+     //The names of the players
+     var firstPlayerName = "Spawn";
+     var secondPlayerName = "Alucard";
 
-    /* This function is invoked after each round and returns a string of either the winner, the loser, a tie (both die)
+     //The players health
+     var firstPlayerHealth = 100;
+     var secondPlayerHealth = 100;
+
+     //players damage
+     var firstPlayerDamage = 15;
+     var secondPlayerDamage = 15;
+
+     var round =1;
+
+     //This function contains the code that loops through rounds and reduces the player's health
+     function fight(){
+
+     };
+
+
+
+     /* This function is invoked after each round and returns a string of either the winner, the loser, a tie (both die)
      or no winner yet. This function does the conditional logic to determine if there is a winner or not.)
      */
-   /* function winnerCheck();       */
-})
+    function winnerCheck(){};
+
+    fight();
+
+})();
